eee1fe0fecc27e8398994a5ac6d04da301a0bb7f
Timothy Le Bon <timothy.lebon@diconium.com>
2019-04-02 15:27:57 +0200

    step 3 starting

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,20 +1,21 @@
-const { ApolloServer } = require('apollo-server');
-const { createStore } = require('./utils');
-const typeDefs = require('./schema');
-const LaunchAPI = require('./datasources/launch');
-const UserAPI = require('./datasources/user');
+const { ApolloServer } = require("apollo-server");
+const { createStore } = require("./utils");
+const typeDefs = require("./schema");
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+const resolvers = require("./resolvers");
 
 const store = createStore();
 
-
-const server = new ApolloServer({ 
-    typeDefs ,
-     dataSources: () => ({
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
     launchAPI: new LaunchAPI(),
-    userAPI: new UserAPI({ store }),
+    userAPI: new UserAPI({ store })
   })
 });
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
+  console.log(`🚀 Server ready at ${url}`);
+});
