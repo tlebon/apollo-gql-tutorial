d0b0c80a50489fc5182e38452c327f3d0e5adbe1
Timothy Le Bon <timothy.lebon@diconium.com>
2019-04-02 16:18:37 +0200

    step 3 completed

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,54 +1,67 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-    type Query {
-  launches: [Launch]!
-  launch(id: ID!): Launch
-  # Queries for the current user
-  me: User
-}
-type Launch {
-  id: ID!
-  site: String
-  mission: Mission
-  rocket: Rocket
-  isBooked: Boolean!
-}
-type Rocket {
-  id: ID!
-  name: String
-  type: String
-}
+  type Query {
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
 
-type User {
-  id: ID!
-  email: String!
-  trips: [Launch]!
-}
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
 
-type Mission {
-  name: String
-  missionPatch(size: PatchSize): String
-}
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
 
-enum PatchSize {
-  SMALL
-  LARGE
-}
-type Mutation {
-  # if false, booking trips failed -- check errors
-  bookTrips(launchIds: [ID]!): TripUpdateResponse!
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+  type Mutation {
+    # if false, booking trips failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
 
-  # if false, cancellation failed -- check errors
-  cancelTrip(launchId: ID!): TripUpdateResponse!
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
 
-  login(email: String): String # login token
-}
-type TripUpdateResponse {
-  success: Boolean!
-  message: String
-  launches: [Launch]
-
-}`
+    login(email: String): String # login token
+  }
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+`;
 
 module.exports = typeDefs;
